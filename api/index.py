283372cf159ebee39c39e4fab41e7a1860083251
Jeff <jeff372001@gmail.com>
2026-02-11 14:15:44 +0630

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,26 +3,40 @@ import random
 
 app = Flask(__name__)
 
-class Card:
-    def __init__(self, suit, rank):
-        self.suit = suit
-        self.rank = rank
-        self.value = 0 if rank in ['J', 'Q', 'K', '10'] else (1 if rank == 'A' else int(rank))
-
-@app.route('/api/deal', methods=['GET'])
-def deal():
+def get_deck():
     suits = ['♠', '♥', '♦', '♣']
     ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-    deck = [{"suit": s, "rank": r, "val": 0 if r in ['J', 'Q', 'K', '10'] else (1 if r == 'A' else int(r))} for s in suits for r in ranks]
+    return [{"suit": s, "rank": r} for s in suits for r in ranks]
+
+def calculate_score(hand):
+    score = 0
+    for card in hand:
+        r = card['rank']
+        if r in ['10', 'J', 'Q', 'K']: score += 0
+        elif r == 'A': score += 1
+        else: score += int(r)
+    return score % 10
+
+@app.route('/api/play', methods=['POST'])
+def play():
+    deck = get_deck()
     random.shuffle(deck)
     
-    # Player နဲ့ Bot ကို ၂ ကတ်စီပေးမယ်
-    res = {
-        "player": [deck.pop(), deck.pop()],
-        "bot": [deck.pop(), deck.pop()],
-        "remaining_deck": deck
-    }
-    return jsonify(res)
+    player_hand = [deck.pop(), deck.pop()]
+    bot_hand = [deck.pop(), deck.pop()]
+    
+    # Simple Bot AI: ၅ မှတ်အောက်ဆို နောက်တစ်ကတ်ဆွဲမယ်
+    bot_score = calculate_score(bot_hand)
+    if bot_score < 5:
+        bot_hand.append(deck.pop())
+        bot_score = calculate_score(bot_hand)
 
-if __name__ == '__main__':
+    return jsonify({
+        "player": player_hand,
+        "bot": bot_hand,
+        "player_score": calculate_score(player_hand),
+        "bot_score": bot_score
+    })
+
+if __name__ == "__main__":
     app.run()
